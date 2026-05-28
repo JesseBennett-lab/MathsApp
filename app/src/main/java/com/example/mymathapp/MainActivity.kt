c262c9bb60f6de08a7982994e272bef1c9632e14
JesseBennett-lab <j.bennett0505@gmail.com>
2026-05-28 13:55:43 +0200

    layout and id updated

---
+++ app/src/main/java/com/example/mymathapp/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.mymathapp
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val birthdayText = findViewById<EditText>(R.id.birthdayText)
+        val totalText = findViewById<TextView>(R.id.totalText)
+        val submitButton = findViewById<Button>(R.id.submitButton)
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
